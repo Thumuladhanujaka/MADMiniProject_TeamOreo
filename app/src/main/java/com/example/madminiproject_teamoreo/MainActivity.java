c3174dedb28532960b43c9bf6c717802a99d432d
Thumula <thumularoxx@gmail.com>
2019-08-05 11:29:25 +0530

    implement your project here

---
+++ app/src/main/java/com/example/madminiproject_teamoreo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.madminiproject_teamoreo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
