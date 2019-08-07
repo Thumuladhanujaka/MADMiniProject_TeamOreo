32cbe34d5d73a6dfcd8bcf629c50f788b9e0274e
danushikaHerath <danushikaherath@gmail.com>
2019-08-07 08:21:09 +0530

    Items Activity

---
+++ app/src/main/java/com/example/madminiproject_teamoreo/ItemsGallery.java
@@ -0,0 +1,14 @@
+package com.example.madminiproject_teamoreo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemsGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_gallery);
+    }
+}
